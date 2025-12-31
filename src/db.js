ee5a5eb056a2e50a13ea342054d857194f16e0a5
austin7313 <austin7313@gmail.com>
2025-12-31 20:24:50 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -7,9 +7,16 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+});
+
+pool.on('connect', () => {
+  console.log('✓ Connected to PostgreSQL');
 });
 
 pool.on('error', (err) => {
@@ -17,3 +24,4 @@ pool.on('error', (err) => {
 });
 
 module.exports = pool;
+
